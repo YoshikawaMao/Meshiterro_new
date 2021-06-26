d1cb114090db8609be9f37b568dcc73dd742e6e6
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-06-26 06:26:49 +0000

    [Finish]4-8,9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
+  # get 'users/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
